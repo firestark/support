b97f50873ca3a32040cbf1410ab155768bee2b4d
Aron Wouters <lionar@mail.com>
2017-10-02 14:32:49 +0200

    Added get_type function for type hint checking.

--- source/helpers/checks.php
+++ source/helpers/checks.php
@@ -1,5 +1,14 @@
 <?php
 
+define ( 'types', 
+[ 
+	'string' 	=> 'string', 
+	'integer' 	=> 'int', 
+	'double' 	=> 'float', 
+	'boolean' 	=> 'bool', 
+	'array' 	=> 'array' 
+] );
+
 if ( ! function_exists ( 'is_html' ) )
 {
     function is_html ( $string ) : bool
@@ -8,5 +17,14 @@ if ( ! function_exists ( 'is_html' ) )
             return false;
             
         return $string !== strip_tags ( $string ) ? true : false;
+    }
+}
+
+if ( ! function_exists ( 'get_type' ) )
+{
+    function get_type ( $variable ) : string
+    {
+        $type = gettype ( $variable );
+    	return types [ $type ];
     }
 }
