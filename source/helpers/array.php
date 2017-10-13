acb216dc37c5d8db255a1f9afb938cc344fa5e4f
Aron Wouters <lionar@mail.com>
2017-10-13 15:18:51 +0200

    Added imploding function removed unused functions.

--- source/helpers/array.php
+++ source/helpers/array.php
@@ -23,3 +23,19 @@ function array_get ( array $subject, $key, $default = null )
 
 	return $current;
 }
+
+/**
+ * Implode with the addition of an alternative last glue
+ * 
+ * @param  string $glue     A string to put in between the array pieces
+ * @param  array  $pieces   The pieces to make the string from
+ * @param  string $lastGlue A string to put in between the first to last and last pieces
+ * @return string           The imploded array.
+ */
+function imploding ( string $glue, array $pieces, string $lastGlue = 'and' ) : string
+{
+	$last  = array_slice ( $array, -1 );
+	$first = join (', ', array_slice ( $array, 0, -1 ) );
+	$both  = array_filter ( array_merge ( array ( $first ), $last ), 'strlen' );
+	return join ( ' ' . $lastGlue . ' ', $both );
+}
--- source/helpers/checks.php
+++ source/helpers/checks.php
@@ -1,51 +1,9 @@
 <?php
 
-define ( 'types', 
-[ 
-	'string' 	=> 'string', 
-	'integer' 	=> 'int', 
-	'double' 	=> 'float', 
-	'boolean' 	=> 'bool', 
-	'array' 	=> 'array',
-	'null'		=> 'null',
-	'NULL'		=> 'null'
-] );
-
-if ( ! function_exists ( 'is_html' ) )
+function is_html ( $string ) : bool
 {
-    function is_html ( $string ) : bool
-    {
-        if ( ! is_string ( $string ) )
-            return false;
-            
-        return $string !== strip_tags ( $string ) ? true : false;
-    }
-}
-
-function get_type ( $variable )
-{   
-	if ( is_string ( $variable ) )
-		$variable = getcorrectvariable ( $variable );
-    $type = gettype ( $variable );
-    	return types [ $type ];
-}
-
-function getcorrectvariable ( $string )
-{
-
-    $string = trim ( $string );
-   
-    if ( empty ( $string ) ) return '';
-    
-    if ( ! preg_match ( "/[^0-9.]+/", $string ) )
-    {
-        if ( preg_match ( "/[.]+/", $string ) )
-            return ( float ) $string;
-        else
-            return ( int ) $string;
-    }
-
-    if ( $string === 'true' ) return true;
-    if ( $string === 'false' ) return false;
-    return ( string ) $string;
+    if ( ! is_string ( $string ) )
+        return false;
+        
+    return $string !== strip_tags ( $string ) ? true : false;
 }
