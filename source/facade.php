590bb17d0e3b9947157d4c0744713821008a6073
Aron Wouters <lionar@mail.com>
2017-10-23 15:50:57 +0200

    Added base facade class.

---
+++ source/facade.php
@@ -0,0 +1,41 @@
+<?php
+
+
+abstract class facade
+{
+    protected static $app = null;
+    protected static $resolvedInstances = [ ];
+    
+    public static function getFacadeRoot ( )
+    {
+        return static::resolveFacadeInstance ( static::getFacadeAccessor ( ) );
+    }
+    
+    protected static function getFacadeAccessor ( )
+    {
+        throw new \runtimeException ( 'Facade does not implement getFacadeAccessor method.' );
+    }
+
+    protected static function resolveFacadeInstance ( $name )
+    {
+        if ( isset ( static::$resolvedInstances [ $name ] ) )
+            return static::$resolvedInstances [ $name ];
+
+        return static::$resolvedInstances [ $name ] = static::$app [ $name ];
+    }
+    
+    public static function setFacadeApplication ( $app )
+    {
+        static::$app = $app;
+    }
+
+    public static function __callStatic ( $method, $args )
+    {
+        $instance = static::getFacadeRoot ( );
+        
+        if ( ! $instance )
+            throw new \runtimeException ( 'A facade root has not been set.' );
+        
+        return $instance->$method ( ...$args );
+    }
+}
