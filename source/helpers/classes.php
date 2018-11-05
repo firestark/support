e8817c1b61367b3dd413455e43b23d59e3713314
Aron Wouters <lionar@mail.com>
2018-11-05 19:45:25 +0100

    Added isset check

--- source/helpers/classes.php
+++ source/helpers/classes.php
@@ -1,13 +1,16 @@
 <?php
 
-/**
- * Get the class "basename" of the given object / class.
- *
- * @param  string|object  $class
- * @return string
- */
-function class_basename ( $class )
+if ( ! function_exists ( 'class_basename' ) )
 {
-	$class = is_object ( $class ) ? get_class ( $class ) : $class;
-	return basename ( str_replace ( '\\', '/', $class ) );
+	/**
+	 * Get the class "basename" of the given object / class.
+	 *
+	 * @param  string|object  $class
+	 * @return string
+	 */
+	function class_basename ( $class )
+	{
+		$class = is_object ( $class ) ? get_class ( $class ) : $class;
+		return basename ( str_replace ( '\\', '/', $class ) );
+	}
 }
