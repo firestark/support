68f9fce2f789d773f14101e787f8b67b57c4932d
Aron Wouters <lionar@mail.com>
2019-06-06 11:09:47 +0200

    Added including function

--- source/helpers/misc.php
+++ source/helpers/misc.php
@@ -9,3 +9,13 @@ function requiring ( string $directory )
 	  	foreach ( $directory as $file )
 	  		require $file;
 }
+
+function including ( string $directory )
+{
+	$directory = new RecursiveDirectoryIterator ( $directory );
+	$iterator = new RecursiveIteratorIterator ( $directory );
+	$objects = new RegexIterator ( $iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH );
+	foreach ( $objects as $directory )
+	  	foreach ( $directory as $file )
+	  		include $file;
+}
